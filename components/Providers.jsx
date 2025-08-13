17b7be4ad9fbd67ea48fefd3c46e711bb7685950
Joshtri <stuffofyos1516@gmail.com>
2025-08-13 19:41:50 +0700

    Commit: Providers.jsx

--- components/Providers.jsx
+++ components/Providers.jsx
@@ -1,19 +1,11 @@
 "use client";
 
-import { ThemeProvider } from "@/providers/themes-provider";
 import { ReactQueryProvider } from "@/providers/ReactQueryProvider";
 import { Toaster } from "sonner";
 
 export function Providers({ children }) {
   return (
-    // <ThemeProvider
-    //   attribute="class"
-    //   defaultTheme="light"
-    //   enableSystem
 
-    //   // disableTransitionOnChange
-    // >
-    // </ThemeProvider>
     <ReactQueryProvider>
       <Toaster richColors position="top-right" />
       {children}
