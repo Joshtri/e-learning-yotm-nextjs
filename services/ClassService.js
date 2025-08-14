fc9a8074d22b66d935355ae0a32f2f63261bb5ce
Joshtri <stuffofyos1516@gmail.com>
2025-08-14 10:01:24 +0700

    Commit: ClassService.js

--- services/ClassService.js
+++ services/ClassService.js
@@ -2,24 +2,24 @@ import api from "@/lib/axios";
 
 // Ambil detail kelas berdasarkan ID
 export const getClassById = async (id) => {
-  const res = await api.get(`/api/classes/${id}`);
+  const res = await api.get(`/classes/${id}`);
   return res.data;
 };
 
 // Update kelas berdasarkan ID
 export const updateClass = async (id, payload) => {
-  const res = await api.patch(`/api/classes/${id}`, payload);
+  const res = await api.patch(`/classes/${id}`, payload);
   return res.data;
 };
 
 // Hapus kelas berdasarkan ID
 export const deleteClass = async (id) => {
-  const res = await api.delete(`/api/classes/${id}`);
+  const res = await api.delete(`/classes/${id}`);
   return res.data;
 };
 
 // Ambil semua data kelas (jika kamu butuh list)
 export const getAllClasses = async () => {
-  const res = await api.get(`/api/classes`);
+  const res = await api.get(`/classes`);
   return res.data;
 };
