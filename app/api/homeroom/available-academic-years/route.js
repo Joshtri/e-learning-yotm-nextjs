68ae5dbb6356adcaf1467bd7be0dce61fe2e0de6
Joshtri <stuffofyos1516@gmail.com>
2025-10-24 07:22:45 +0700

    Commit: route.js

--- app/api/homeroom/available-academic-years/route.js
+++ app/api/homeroom/available-academic-years/route.js
@@ -40,7 +40,7 @@ export async function GET(request) {
       },
       orderBy: [
         { academicYear: { tahunMulai: "desc" } },
-        { academicYear: { semester: "asc" } },
+        { academicYear: { semester: "desc" } }, // GENAP first, then GANJIL
       ],
     });
 
